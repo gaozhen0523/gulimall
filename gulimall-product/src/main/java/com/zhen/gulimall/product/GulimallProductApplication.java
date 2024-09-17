4375bb46028f9424337e650b8190e32a7ab23d59
gaozhen0523 <gaozhen0523@gmail.com>
2024-09-16 21:28:32 -0500

    register service to nacos center

--- gulimall-product/src/main/java/com/zhen/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/zhen/gulimall/product/GulimallProductApplication.java
@@ -4,8 +4,10 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.zhen.gulimall.product.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallProductApplication {
 
