a50ee7ac0a0863ce94fbdc3e4f115be7b478893d
gaozhen0523 <gaozhen0523@gmail.com>
2024-09-19 20:21:25 -0500

    set up gateway

--- gulimall-product/src/main/java/com/zhen/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/zhen/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.zhen.gulimall.product.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,38 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1.find all categories
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        //2.load up as tree
+
+        //2.1 find all first-level
+        List<CategoryEntity> level1Menus = entities.stream().filter(categoryEntity -> categoryEntity.getParentCid() == 0
+        ).map((menu)->{
+            menu.setChildren(getChildren(menu,entities));
+            return menu;
+        }).sorted((menu1,menu2)->{
+            return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0: menu2.getSort());
+        }).collect(Collectors.toList());
+
+        return level1Menus;
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all){
+
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid() == root.getCatId();
+        }).map((menu)->{
+            menu.setChildren(getChildren(menu,all));
+            return menu;
+        }).sorted((menu1,menu2)->{
+            return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0: menu2.getSort());
+        }).collect(Collectors.toList());
+
+        return children;
     }
 
 }
