7d0f0a9c68573b1e93afaf4da5039fbf8ce5b29b
gaozhen0523 <gaozhen0523@gmail.com>
2024-11-11 19:44:14 -0600

    upload to ali cloud

--- gulimall-product/src/test/java/com/zhen/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/zhen/gulimall/product/GulimallProductApplicationTests.java
@@ -1,5 +1,6 @@
 package com.zhen.gulimall.product;
 
+import com.aliyun.oss.OSSClient;
 import com.zhen.gulimall.product.entity.BrandEntity;
 import com.zhen.gulimall.product.service.BrandService;
 import org.junit.Test;
@@ -8,12 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GulimallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+
+    @Autowired
+    OSSClient ossClient;
+
     @Test
     public void contextLoads() {
 
@@ -21,6 +30,18 @@ public class GulimallProductApplicationTests {
         brandEntity.setDescript("");
         brandEntity.setName("huawei");
         brandService.save(brandEntity);
+    }
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+
+        InputStream inputStream = new FileInputStream("C:\\Users\\gaozh\\Desktop\\pics\\DigitalPhoto.jpg");
+
+        ossClient.putObject("gulimall-hello", "DigitalPhoto.jpg", inputStream);
+
+        ossClient.shutdown();
+
+        System.out.println("upload finished");
     }
 
 }
